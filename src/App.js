b5a91458bb9733c922db6dbbdf204481902b5abd
Rifkhankhan <you@example.comclscls>
2023-04-11 20:14:55 +0530

    finish most of the works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
+import { BrowserRouter, Outlet, Route, Router, Routes } from 'react-router-dom';
+import Routers from './routers/Routers'
+
 import './App.css';
+import Topbar from './Components/topbar/Topbar';
+import { useEffect, useState } from 'react';
+import RightSideCardModel from './Components/RightSideCardModel/RightSideCardModel';
 
 function App() {
+	const [searchItems,setSearchItems] = useState()
+  const [model,setModel] = useState(false)
+  useEffect(() => {
+    if(model) {
+      
+      document.documentElement.style.overflow = 'hidden';
+      document.body.scroll = "no";
+    }
+    else{
+      document.documentElement.style.overflow = 'scroll';
+      document.body.scroll = "yes";
+    }
+
+    setModel(model)
+
+  },[model])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Topbar setModel={setModel} setSearchItems={setSearchItems}/>
+          <Routers searchItems={searchItems}/>
+          {/* <Outlet /> */}
+        <RightSideCardModel model={model} setModel={setModel} />
+      </BrowserRouter>
+
     </div>
   );
 }
